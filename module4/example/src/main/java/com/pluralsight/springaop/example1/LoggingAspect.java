9b478bc11a72fad6cdaac42f6a97e797973b20a9
ye Li <yeli.88@gmail.com>
2021-04-28 16:24:04 -0400

    around using JDK Dynamic proxy

---
+++ module4/example/src/main/java/com/pluralsight/springaop/example1/LoggingAspect.java
@@ -0,0 +1,26 @@
+package com.pluralsight.springaop.example1;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+import java.util.logging.Logger;
+
+@Aspect
+public class LoggingAspect {
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+
+    @Around("execution(* *.*Passenger(..))")
+    public Object log (ProceedingJoinPoint thisJointPoint) throws  Throwable{
+        String methodName = thisJointPoint.getSignature().getName();
+        Object[] methodArgs = thisJointPoint.getArgs();
+        logger.info("Call method " + methodName + " with arg " + methodArgs[0]);
+        Object result = thisJointPoint.proceed();
+        logger.info("Method " + methodName + " returns " + result);
+        return result;
+    }
+
+
+}
