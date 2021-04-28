13dd681c9e9dc763274afc3343aeec730d377db8
ye Li <yeli.88@gmail.com>
2021-04-28 16:49:13 -0400

    CGLIB proxy will not work if the target method in target class is final

---
+++ module7/example/src/main/java/com/pluralsight/springaop/example1/PassengersManager.java
@@ -0,0 +1,16 @@
+package com.pluralsight.springaop.example1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PassengersManager {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("example1/aop.xml");
+
+        PassengerDaoImpl passengerDao = (PassengerDaoImpl) context.getBean("passengerDao");
+        System.out.println(passengerDao.getPassenger(1));
+
+        context.close();
+    }
+}
