2ec25789b6e03b2c3b00525b6fe392b802943c28
ye Li <yeli.88@gmail.com>
2021-04-28 16:35:03 -0400

    around using CGLIB proxy

---
+++ module5/example/src/main/java/com/pluralsight/springaop/example1/PassengerDaoImpl.java
@@ -0,0 +1,20 @@
+package com.pluralsight.springaop.example1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassengerDaoImpl //implements PassengerDao
+{
+
+    private static Map<Integer, Passenger> passengerMap = new HashMap<Integer, Passenger>();
+
+    public Passenger getPassenger(int id) {
+        if (null != passengerMap.get(id)){
+            return passengerMap.get(id);
+        }
+
+        Passenger passenger = new Passenger(id);
+        passengerMap.put(id, passenger);
+        return passenger;
+    }
+}
