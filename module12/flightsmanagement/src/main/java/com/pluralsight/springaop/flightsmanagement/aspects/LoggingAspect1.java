82bab81b5371053acf717d76c930fb528f8e7c5d
ye Li <yeli.88@gmail.com>
2021-04-30 15:55:18 -0400

    @Before, @After and @Pointcut

---
+++ module12/flightsmanagement/src/main/java/com/pluralsight/springaop/flightsmanagement/aspects/LoggingAspect1.java
@@ -0,0 +1,33 @@
+package com.pluralsight.springaop.flightsmanagement.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+
+import java.util.logging.Logger;
+
+@Aspect
+public class LoggingAspect1 {
+    private Logger logger =
+            Logger.getLogger(LoggingAspect1.class.getName());
+
+    @Before("execution(public String com.pluralsight.springaop.flightsmanagement.domain.Flight.getId())")
+    public void loggingAdviceGetId() {
+        logger.info("Flight getId method will be called");
+    }
+
+    @AfterReturning("execution(public * *.print())")
+    public void LoggingAdvicePrint(){
+        logger.warning("A print method has been called");
+    }
+
+    @Pointcut("within(com.pluralsight.springaop.flightsmanagement.domain.Ticket)")
+    public void allTicketMethods() {
+    }
+
+    @After("allTicketMethods()")
+    public void loggingAdvice(JoinPoint joinPoint){
+        logger.info("A ticket method had been called.");
+        logger.info(joinPoint.toString());
+        logger.info(joinPoint.getTarget().toString());
+    }
+}
---
+++ module12/flightsmanagement/src/main/java/com/pluralsight/springaop/flightsmanagement/domain/Ticket.java
@@ -0,0 +1,32 @@
+package com.pluralsight.springaop.flightsmanagement.domain;
+
+public class Ticket {
+
+    private String number;
+    private Passenger passenger;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public String emittingCountry() {
+        return passenger.getCountry();
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket number: " + number;
+    }
+}
